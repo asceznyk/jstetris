407569d5eb9a527a1ed06b630caa2619c3d865e1
asceznyk <asceznyk@gmail.com>
2021-05-04 19:44:49 +0530

    added countHoles

--- ai.js
+++ ai.js
@@ -1,16 +1,19 @@
 var	heightBumpiness = function(arena) {
 	let agheight = 0;
-	let bumpiness = 0;
+	let bumpiness = 0;	
+
 	let last = 0;
 	for(let c = 0; c < arena.cols; c++) {
 		for(let r = 0; r < arena.rows; r++)  {	
 			if(arena.matrix[r][c] > 0) {
 				let height = arena.rows - r;
 				agheight += height;
+				
 				if(last) {
 					bumpiness += Math.abs(last - height);
 				}
 				last = height;
+
 				break;
 			}
 		}
@@ -34,10 +37,27 @@ var completeLines = function(arena) {
 	return lines;
 }
 
+var countHoles = function(arena) {
+	let holes = 0;
+	for(let c = 0; c < arena.cols; c++) {
+		for(let r = 0; r < arena.rows; r++) {
+			if(arena.matrix[r][c] > 0) {
+				if(arena.matrix[r+1] && arena.matrix[r+1][c] == 0) {
+					holes++;	
+				}		
+			}
+		}
+	}
+
+	return holes;
+}
+
+
 var arenaScore = function(arena) {
 	let [agheight, bumpiness] = heightBumpiness(arena);
 	let lines = completeLines(arena);
-	console.log(agheight, bumpiness, lines);
+	let holes = countHoles(arena);
+	console.log(agheight, bumpiness, holes, lines);
 }
 
 
